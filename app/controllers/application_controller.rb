fb962cf9d48059f9f60ee0adfe19ac60efbc6aab
HumphreyB612 <86584350+HumphreyB612@users.noreply.github.com>
2024-12-02 20:26:10 +0800

    修改了注释

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # class ApplicationController < ActionController::Base
 # app/controllers/application_controller.rb
+# 程序入口
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :exception
